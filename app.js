805c547191c4e1f68bf0e06d95f2e4b3447f5398
YuriGGG <yurijp95@hotmail.com>
2022-05-30 11:49:26 -0500

    REMASTERIZADO

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
 const port = process.env.PORT || 3000; //Heroku conection
 
 app.listen(port,() => console.log('SERVIDOR FUNCIONANDO AL 100'));
 
-app.use (express.static(path.join(__dirname, './public')));
+
+app.use('/static', express.static(__dirname + '/public'));  //CARPETA PUBLICA/ESTATICA
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname + '/views/home.html'));
-});
-
-app.get('/register', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/register.html'))
+    res.sendFile(__dirname + '/views/home.html');
 });
 
 app.get('/login', (req,res)=>{
     res.sendFile(path.join(__dirname,'/views/login.html'))
+})
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/register.html'))
 })
