7814d44cc28a1e3866266101f7419d10db06ead7
YuriGGG <yurijp95@hotmail.com>
2022-05-30 10:50:42 -0500

    Cambios y cambios

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000; //Heroku conection
 
 app.listen(port,() => console.log('SERVIDOR FUNCIONANDO AL 100'));
 
-app.use (express.static(path.join(__dirname,'./public')));
+app.use (express.static(path.join(__dirname, './public')));
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname + '/views/home.html'));
