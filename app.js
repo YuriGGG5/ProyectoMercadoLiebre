7532ef9bfd5dd9bbccb21743edb7d44a5b6a8e62
YuriGGG <yurijp95@hotmail.com>
2022-05-30 09:45:07 -0500

    comentarios finales

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
 
-app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, './public')));
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log('Servidor funcionando');
-});
+const port = process.env.PORT || 3000;
+
+app.listen(port,() => console.log('SERVIDOR FUNCIONANDO AL 100'));
+
+app.use (express.static(path.join(__dirname,'./public')));
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(path.join(__dirname + '/views/home.html'));
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/register.html'))
 });
 
 app.get('/login', (req,res)=>{
     res.sendFile(path.join(__dirname,'/views/login.html'))
-})
-app.get('/register', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/register.html'))
 })
